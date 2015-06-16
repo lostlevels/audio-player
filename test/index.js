74386c89efd763e38505cf0607e54c5e3dc118f6
irina <shestak.irina@gmail.com>
2015-06-16 12:37:07 -0700

    swap playlists prototype

--- test/index.js
+++ test/index.js
@@ -1,20 +1,6 @@
 var AudioPlayer = require("audio-player");
 
 var ap = new AudioPlayer();
-ap.add({source:"./1 PM.mp3"});
-ap.add({source:"./2 PM.mp3"});
-ap.add({source:"./3 PM.mp3"});
-
-//var ap = new AudioPlayer();
-
-//var ap3 = new AudioPlayer();
-//ap.add({source:"./glass0.mp3"});
-//ap.add({source:"./glass1.mp3"});
-//ap.add({source:"./glass2.mp3"});
-//ap.add({source:"./glass3.mp3"});
-//ap.add({source:"./glass4.mp3"});
-//ap.add({source:"./glass5.mp3"});
-
 ap.add({source:"./Op. 9 No. 1.mp3"});
 ap.add({source:"./Op. 9 No. 2.mp3"});
 ap.add({source:"./Op. 9 No. 3.mp3"});
@@ -24,23 +10,20 @@ ap.add({source:"./Op. 27 No. 1.mp3"});
 ap.add({source:"./Op. 27 No. 2.mp3"});
 ap.add({source:"./Op. 32 No. 1.mp3"});
 ap.add({source:"./Op. 32 No. 2.mp3"});
-ap.add({source:"./Op. 37 No. 1.mp3"});
-ap.add({source:"./Op. 37 No. 2.mp3"});
-ap.add({source:"./Op. 48 No. 1.mp3"});
-ap.add({source:"./Op. 48 No. 2.mp3"});
-ap.add({source:"./Op. 55 No. 1.mp3"});
-ap.add({source:"./Op. 55 No. 2.mp3"});
-ap.add({source:"./Op. 62 No. 1.mp3"});
-ap.add({source:"./Op. 62 No. 2.mp3"});
-ap.add({source:"./Op. 72 No. 1.mp3"});
+
+var ap2 = new AudioPlayer();
+ap2.add({source:"./Op. 37 No. 1.mp3"});
+ap2.add({source:"./Op. 37 No. 2.mp3"});
+ap2.add({source:"./Op. 48 No. 1.mp3"});
+ap2.add({source:"./Op. 48 No. 2.mp3"});
+ap2.add({source:"./Op. 55 No. 1.mp3"});
+ap2.add({source:"./Op. 55 No. 2.mp3"});
+ap2.add({source:"./Op. 62 No. 1.mp3"});
+ap2.add({source:"./Op. 62 No. 2.mp3"});
+ap2.add({source:"./Op. 72 No. 1.mp3"});
 
 ap.addEventListener('play', createAudioListeners);
 ap.play();
-
-//var playlists = [];
-//playlists.push(ap.items);
-//playlists.push(ap2.items);
-//playlists.push(ap3.items);
 
 //buttons to respond to their individual actions; 
 var button = document.getElementsByTagName("button");
@@ -85,15 +68,12 @@ function toggleAttribute (attribute) {
   }
 };
 
-//function togglePlaylist (element) {
-//  var currentPlaylist = playlists[0];
-//  var playlistLength = playlists.length;
-//  for (var i = 0; i < playlistLength; i++) {
-//    if (currentPlaylist === playlists[playlistLength-1]) { // restart playlist
-//      currentPlaylist = playlists[0];
-//    } else if (currentPlaylist === playlists[i]) {
-//      currentPlaylist = playlists[i+1];
-//    }
-//  }
-//  ap[element](currentPlaylist);
-//};
+function togglePlaylist (element) {
+  var currentPlaylist = ap.items; 
+  if (currentPlaylist === ap.items) { // restart playlist
+    currentPlaylist = ap2.items;
+  } else {
+    currentPlaylist = ap.items;
+  }
+  ap[element](currentPlaylist);
+};
